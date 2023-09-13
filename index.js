7576408b8726dabe9b64b973558b58c8254e2254
Ashis Kumar <srimanashis@gmail.com>
2023-09-13 17:26:24 +0400

    OCR PDF

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get("/ocr-pdf", async (req, res) => {
 			/* console.log('stdout:', stdout);
 			console.log('stderr:', stderr); */
 
-			await exec(`ocrmypdf './uploads/${outputFile}' './uploads/${outputFile}'`);
+			await exec(`ocrmypdf --rotate-pages --remove-background --deskew --clean --clean-final './uploads/${outputFile}' './uploads/${outputFile}'`);
 
 			res.status(200).json({ isSuccessful: true, file: outputFile })
 		} catch (error) {
